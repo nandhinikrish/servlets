b4538daf0826c8abd108dc81b6e0c1c2cbe4c8ce
nandhinikrish <62050090+nandhinikrish@users.noreply.github.com>
2020-08-03 22:21:44 +0530

    Add files via upload

---
+++ Sql query.sql
@@ -0,0 +1,10 @@
+
+create table Bankdetails(
+Accountholder varchar(15),
+Accountnumber integer(12),
+IFScode varchar(15),
+BranchName varchar(25),
+Amount integer(7)
+);
+insert into Bankdetails values("somu","123456789","IDIB002345","indore branch",15000);
+insert into Bankdetails values("ramu","978456123","IDIB002145"," chennai branch",12000);
